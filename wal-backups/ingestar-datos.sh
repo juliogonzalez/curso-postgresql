bf3c9d4e8c6f350b0ed9e5711e5355b5463fd1fe
Julio Gonzalez Gil <git@juliogonzalez.es>
2018-08-05 01:49:28 +0200

    Mostar pérdida de datos con WAL PITR añadindo más tablas

--- wal-backups/ingestar-datos.sh
+++ wal-backups/ingestar-datos.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo -u postgres psql testdb -c 'DROP TABLE IF EXISTS test; CREATE TABLE TEST (id SERIAL PRIMARY KEY, timestamp BIGINT NOT NULL)'
 for i in $(seq 1 1000000); do echo ${i}; done | sudo -u postgres psql testdb -c 'COPY test (timestamp) FROM STDIN;'
+sudo -u postgres psql testdb -c 'DROP TABLE IF EXISTS test2; CREATE TABLE TEST2 (id SERIAL PRIMARY KEY, timestamp BIGINT NOT NULL)'
+for i in $(seq 1 1000000); do echo ${i}; done | sudo -u postgres psql testdb -c 'COPY test2 (timestamp) FROM STDIN;'
+sudo -u postgres psql testdb -c 'DROP TABLE IF EXISTS test3; CREATE TABLE TEST3 (id SERIAL PRIMARY KEY, timestamp BIGINT NOT NULL)'
+for i in $(seq 1 1000000); do echo ${i}; done | sudo -u postgres psql testdb -c 'COPY test3 (timestamp) FROM STDIN;'
